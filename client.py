42a34f8a747fd2dfd9dd088af300f64bb6ecbeef
Aferists <ageristilya@gmail.com>
2021-07-14 22:14:01 +0300

    socet

--- client.py
+++ client.py
@@ -2,26 +2,27 @@ import urllib.request
 import os
 import base64
 import json
-import socet
-import sys
+import socket
+import zlib
+import threading
 
 def main():
     print('1) Upload a file to the server')
     print('2) Display all firmware')
     print('3) Filtration')
     print('4) Search')
-    vvod = input('Choose: ')
-    menu(int(vvod))
+    choose = input('Choose: ')
+    menu(int(choose))
 
 
-def menu(vvod):
-    if vvod == 1:
+def menu(choose):
+    if choose == 1:
         sockets()
-    elif vvod == 2:
+    elif choose == 2:
         list()
-    elif vvod == 3:
+    elif choose == 3:
         filtr()
-    elif vvod == 4:
+    elif choose == 4:
         find()
     else:
         print('invalid input')
@@ -32,15 +33,36 @@ def sockets():
     platform = input('Platform: ')
     scope = input('Scope: ')
     version = input('Version: ')
+    desc = input('Description: ')
     path_file = input('Enter the path to the file: ')
-    sock = socket.socket()
-    sock.connect(('localhost', 8000))
+
+    host = '127.0.0.1'
+    port = 0
+    server = ("127.0.0.1", 9090)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    sock.setblocking(0)
+    rT = threading.Thread(args=("RecvThread", sock))
+    rT.start()
+
     fopen = open(f'{path_file}', 'rb')
-    lists = f.read()
-    while (lists):
+    lists = fopen.read()
+    fileSumCheck = hex(zlib.crc32(lists))
+    size = len(lists)
+
+    sock.sendto(("Upload").encode("utf-8"), server)
+    sock.sendto((scope).encode("utf-8"), server)
+    sock.sendto((platform).encode("utf-8"), server)
+    sock.sendto((version).encode("utf-8"), server)
+    sock.sendto((desc).encode("utf-8"), server)
+    sock.sendto((fileSumCheck).encode("utf-8"), server)
+    sock.sendto((size).encode("utf-8"), server)
+    while lists:
         sock.send(lists)
         lists = fopen.read()
     sock.close()
+
 
 def list():
     url = f'http://localhost:8000/get'
@@ -61,8 +83,8 @@ def list():
     print('1) Download file')
     print('2) contain')
     print('3) exit')
-    vvod = input('Choose: ')
-    if int(vvod) == 1:
+    choose = input('Choose: ')
+    if int(choose) == 1:
         download_firmware = input('Choose which firmware to download: ')
         i = int(download_firmware) - 1
 
@@ -84,9 +106,9 @@ def list():
         items_download = json_obj_download['downloadUrl']
 
         download_file(items_download)
-    elif int(vvod) == 2:
+    elif int(choose) == 2:
         main()
-    elif int(vvod) == 3:
+    elif int(choose) == 3:
         exit(0)
     else:
         print('Invalid input')
@@ -95,9 +117,17 @@ def list():
 
 def filtr():
     platform = input('Platform (Android, iOS Any): ')
-    version = input('Version: ')
     scope = input('Scope: ')
-    url = f'http://localhost:8000/get?platform={platform}&version={version}&scope={scope}'
+    print('1) Поиск между версиями')
+    print('2) Найти версию')
+    choos = input('Choose')
+    if int(choos) == 1:
+        lversion = input('Введите минимальную версию: ')
+        rversion = input('Введите максимальную версию: ')
+        url = f'http://localhost:8000/get?platform={platform}&lversion={lversion}&rversion={rversion}&scope={scope}'
+    elif int(choos) == 2:
+        version = input('Version: ')
+        url = f'http://localhost:8000/get?platform={platform}&version={version}&scope={scope}'
 
     request = urllib.request.Request(url)
     base64string = base64.b64encode(bytes('%s:%s' % ('user', 'user'), 'ascii'))
@@ -111,14 +141,14 @@ def filtr():
         items_platform = items_full[i]['platform']
         items_version = items_full[i]['version']
         items_scope = items_full[i]['scope']
-        print(i + 1, f'Platform: {items_platform}, scope: {items_scope}, version: {items_version}, ')
+        print(i + 1, f'Platform: {items_platform}, scope: {items_scope}, version: {items_version}')
     print('\n')
 
     print('1) Download file')
     print('2) contain')
     print('3) exit')
-    vvod = input('Choose: ')
-    if int(vvod) == 1:
+    choose = input('Choose: ')
+    if int(choose) == 1:
         download_firmware = input('Choose which firmware to download: ')
         i = int(download_firmware) - 1
 
@@ -140,9 +170,9 @@ def filtr():
         items_download = json_obj_download['downloadUrl']
 
         download_file(items_download)
-    elif int(vvod) == 2:
+    elif int(choose) == 2:
         main()
-    elif int(vvod) == 3:
+    elif int(choose) == 3:
         exit(0)
     else:
         print('Invalid input')
